f0ba973ac8f8425a9ae8c3349bc6ccfbe6877ef2
deber4566 <86329651+deber4566@users.noreply.github.com>
2025-06-15 20:42:59 -0700

    Add files via upload
    6/15/25

---
+++ Lox.java
@@ -0,0 +1,102 @@
+package lox.lox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+//Lox is dynamically typed
+// REPL . READ, EVALUATE, PRINT, LOOP
+
+//ch. 9
+
+public class Lox{
+
+    //making this final will allow the REPL to use same interpreter object
+    private static final Interpreter interpreter = new Interpreter();
+    static boolean hadError = false;
+    static boolean hadRuntimeError = false;
+
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+
+        // note that these exit messages are custom defined
+        if (hadError) {
+            System.exit(65);
+        }
+
+        if (hadRuntimeError) {
+            System.exit(70);
+        }
+    }
+
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        //infinite for loop for(;;)
+        for (;;) {
+            System.out.print("> ");
+            String line = reader.readLine();
+            if (line == null){
+                break;
+            }
+
+            run(line);
+            hadError = false;
+        }
+    }
+
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+        Parser parser = new Parser(tokens);
+        List<Stmt> statements = parser.parse();
+        //for (Token token: tokens) {       THIS IS OLD CODE
+           // System.out.println(token);
+        //}
+
+        if (hadError) {
+            return;
+        }
+        // to be deleted System.out.println(new AstPrinter().print(expression));
+        interpreter.interpret(statements);
+    }
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.println("[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
+    }
+
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
+    }
+}
